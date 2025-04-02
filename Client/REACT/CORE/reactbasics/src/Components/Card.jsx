2ccada8abca23ad6ecff86052a2ab52d6a208191
Cybersayak <sayakcse2@gmail.com>
2025-04-02 23:41:58 +0530

    Multiple Card Components

--- Client/REACT/CORE/reactbasics/src/Components/Card.jsx
+++ Client/REACT/CORE/reactbasics/src/Components/Card.jsx
@@ -1,18 +1,45 @@
 import React from 'react'
 
 function Card() {
+  const data =[
+    {image: "https://wallpaperaccess.com/full/3949076.jpg" ,name : 'React Basics', description: 'React can change how you think about the designs you look at and the apps you build.'},
+    {image: "https://wallpapersafari.com/download/M0rP6h/",name : 'React Native', description: 'React Native is a framework for building native-style applications for both iOS and Android using JavaScript.'}, 
+    {image: "https://miro.medium.com/max/762/1*zgbi_fJN2p-VMvCvwin8-g.png",name : 'Expo', description: 'Expo is a framework that makes developing Android and iOS apps easier.'},
+    {image: "https://cdn.filestackcontent.com/8MbtJ4hTAaOk3KPcptqZ",name : 'NextJS', description: 'Next.js is a React framework for building full-stack web applications'},
+
+  ];
+  
   return (
-    <div className='w-full h-screen bg-zinc-200'>
-      <div className='w-52 bg-zinc-100 absolute left-1/2 top-1/2 -translate-x-[50%] -translate-y-[50%] rounded-md overflow-hidden'>
+    <div className='w-full h-screen bg-zinc-200 flex justify-center items-center gap-10'>
+      { data.map((elem, index) => (
+        <div key={index} className='w-52 bg-zinc-10 rounded-md overflow-hidden'>
         <div className='w-full h-32 bg-zinc-300'>
-          <img className='w-full h-full object-cover' src="https://wallpaperaccess.com/full/3949076.jpg "></img>
-           </div>
+          <img className='w-full h-full object-cover' src={elem.image}></img>
+        </div>
         <div className='w-full px-3 py-4 '></div>
-        <h2 className='font-semibold m-2 '>React Basics </h2>
-        <p className='text-xs mt-1 m-2 '>React can change how you think about the designs you look at and the apps you build. </p>
+          <h2 className='font-semibold m-2 '>{elem.name} </h2>
+          <p className='text-xs mt-1 m-2 '>{elem.description} </p>
       </div>
+    ))}
     </div>
-  )
+  );
 }
 
 export default Card;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Client/REACT/README.md
@@ -0,0 +1,2 @@
+
+### Components (CARD) & Resuabilty
